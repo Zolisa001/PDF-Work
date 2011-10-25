ffaeaa4e9785184bdbeb3d11fc9417a911ccc061
joshuacc <josh@designpepper.com>
2011-10-25 13:24:59 -0400

    Sketched out basic logic of the form.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$('#add').click(openAddDialog);
+
+	$('#dialog #submit').click(saveStudent);
 });
 
 function openAddDialog() {
@@ -7,4 +9,26 @@ function openAddDialog() {
 		title: "Add Student",
 		width: 400
 	});
+}
+
+function saveStudent() {
+	var form = $('#student-form');
+	var form_action = $(form).attr('action');
+
+	if (form_action == 'add') {
+		addNewStudent();
+	} else if (form_action == 'edit') {
+		editStudent();
+	}
+
+	$('#dialog').dialog('close');
+	$(form).preventDefault();
+}
+
+function addNewStudent() {
+	var student = {};
+}
+
+function saveStudent() {
+	
 }
